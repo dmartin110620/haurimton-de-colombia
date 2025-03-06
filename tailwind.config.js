ee4d99f10d4dcc67b5800f03e60ed0221398d5f4
dmartin110620 <109118261+dmartin110620@users.noreply.github.com>
2025-03-06 00:44:06 -0500

    solved index style issues for all devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/styles/**/*.css", // Includes all CSS files if needed
     ],
     theme: {
-    extend: {},
+    extend: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
+    },
     },
     plugins: [],
 };
